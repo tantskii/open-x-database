a85ea6c67d06e05e3a14f5bdfb7f338338dd5359
Tikhon Sergienko <t.sergienko@visionlabs.ru>
2022-04-27 00:22:32 +0300

    set db params

--- server/main.cpp
+++ server/main.cpp
@@ -3,10 +3,17 @@
 #include "Server.h"
 
 int main() {
-	uint16_t port = 3132;
-	uint16_t numThreads = 4;
+	const uint16_t port = 3132;
+	const uint16_t numThreads = 4;
+	const std::string databaseDirectory = "/tmp/omxdb";
 
-	auto server = omx::Server(port, numThreads);
+	omx::Options databaseOptions = {};
+	databaseOptions.maxMemTableSize  = 1 * 1024 * 1024;
+	databaseOptions.maxWalBufferSize = 1 * 1024;
+	databaseOptions.hashType         = omx::HashType::CityHash128;
+	databaseOptions.compressionType  = omx::CompressionType::LZ4;
+
+	auto server = omx::Server(port, numThreads, databaseDirectory, databaseOptions);
 
 	server.start();
 }
